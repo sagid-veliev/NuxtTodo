857049ef34990a68deeeff9a1d7e27318a0d153c
sagid.veliev <dagfarid73@gmail.com>
2022-08-12 06:04:44 +0400

    Подгружен препроцессор SCSS, создан дефолтный layout, написан компонент Header c заголовком и селектом.

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="container_content">
+            <Header />
+            <Nuxt />
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+
+export default {
+    components: {
+        Header
+    }
+}    
+
+</script>
+
+<style lang="scss">
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    .container {
+        display: grid;
+        max-width: 1440px;
+        height: 900px;
+        background: $theme-color;
+        border: 1px solid black;
+        margin: auto;
+        box-sizing: border-box;
+        &_content {
+            margin: 32px;
+            grid-gap: 16px;
+        }
+    }
+    
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  layout: 'default'
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
